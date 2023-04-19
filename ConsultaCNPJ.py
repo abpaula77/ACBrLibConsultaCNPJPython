cae8571f0ad74c4caf27a3e92e5ac526b8e3808a
Alexandre Ballestero de Paula <82414677+abpaula77@users.noreply.github.com>
2023-04-19 09:04:48 -0300

    atualização de consulta do cnpj

--- ConsultaCNPJ.py
+++ ConsultaCNPJ.py
@@ -1,30 +1,28 @@
 import ctypes
+from PIL import Image
 
 # Carregando a DLL ACBrLibConsultaCNPJ
 acbr_lib = ctypes.windll.LoadLibrary(r'D:\ACBrLibConsultaCNPJ\ACBrConsultaCNPJ64.dll')
 
 inicializa = acbr_lib.CNPJ_Inicializar(r'D:\ACBrLibConsultaCNPJ\ACBrConsultaCNPJ.INI'.encode("utf-8"),"".encode("utf-8"))
 
-# Definindo o tipo de retorno da função CNPJ_Consultar
-acbr_lib.CNPJ_Consultar.restype = ctypes.c_char_p
-
-# Definindo os tipos dos parâmetros da função CNPJ_Consultar
-acbr_lib.CNPJ_Consultar.argtypes = [ctypes.c_char_p, ctypes.c_bool, ctypes.c_char_p, ctypes.c_int]
-
-eCNPJ = b"18760540000139"
-
 retorno = acbr_lib.CNPJ_ConsultarCaptcha(r'D:\ACBrLibConsultaCNPJ'.encode("utf-8"),"".encode("utf-8"),"".encode("utf-8"))
 
-eCAPCHA = b"2qh6ps"
+img = Image.open("CaptchaCNPJ.png")
+img.show()
+
+eCAPCHA = input('Digite o captcha:')
+
+sResposta = "".encode("utf-8")
 
 # Realizando a consulta do CNPJ
-resultado = acbr_lib.CNPJ_Consultar(eCNPJ, eCAPCHA, b"", 0)
+resultado = acbr_lib.CNPJ_Consultar(r'18760540000139'.encode("utf-8"), eCAPCHA.encode("utf-8"), sResposta,0)
 
 # Verificando se a consulta foi bem sucedida
-if resultado.startswith(b"OK"):
+if resultado == 0:
     # Extraindo os dados do CNPJ consultado
-    resultado = resultado.decode("utf-8")
-    dados = resultado.split("|")
+    sResposta = sResposta.decode("utf-8")
+    dados = sResposta.split("|")
 
     # Exibindo os dados do CNPJ consultado
     print("Razão social: ", dados[1])
@@ -32,4 +30,4 @@ if resultado.startswith(b"OK"):
     print("Endereço: ", dados[4])
 else:
     # Exibindo o erro ocorrido na consulta
-    print("Erro na consulta: ", resultado.decode("utf-8"))
+    print("Erro na consulta: ", resultado)
