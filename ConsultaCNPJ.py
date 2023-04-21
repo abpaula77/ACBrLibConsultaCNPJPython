2d6ace448cbfdc293fe752f6cf3c37461a0682ab
Alexandre Ballestero de Paula <82414677+abpaula77@users.noreply.github.com>
2023-04-21 09:37:54 -0300

    ajustes de teste de operação

--- ConsultaCNPJ.py
+++ ConsultaCNPJ.py
@@ -2,7 +2,7 @@ import ctypes
 from PIL import Image
 
 # Carregando a DLL ACBrLibConsultaCNPJ
-acbr_lib = ctypes.windll.LoadLibrary(r'D:\ACBrLibConsultaCNPJ\ACBrConsultaCNPJ64.dll')
+acbr_lib = ctypes.cdll.LoadLibrary(r'D:\ACBrLibConsultaCNPJ\ACBrConsultaCNPJ64.dll')
 
 inicializa = acbr_lib.CNPJ_Inicializar(r'D:\ACBrLibConsultaCNPJ\ACBrConsultaCNPJ.INI'.encode("utf-8"),"".encode("utf-8"))
 
@@ -13,21 +13,34 @@ img.show()
 
 eCAPCHA = input('Digite o captcha:')
 
-sResposta = "".encode("utf-8")
+sResposta = r"".encode("utf-8")
+
+Tamanho = 0
 
 # Realizando a consulta do CNPJ
-resultado = acbr_lib.CNPJ_Consultar(r'18760540000139'.encode("utf-8"), eCAPCHA.encode("utf-8"), sResposta,0)
+resultado = acbr_lib.CNPJ_Consultar(r'18760540000139'.encode("utf-8"), 
+                                    eCAPCHA.encode("utf-8"), 
+                                    sResposta,
+                                    Tamanho)
 
-# Verificando se a consulta foi bem sucedida
+print(resultado)
+print(sResposta.decode("utf-8"))
+
 if resultado == 0:
-    # Extraindo os dados do CNPJ consultado
-    sResposta = sResposta.decode("utf-8")
-    dados = sResposta.split("|")
+    resultado = acbr_lib.CNPJ_UltimoRetorno( sResposta,
+                                         Tamanho)
+    # Verificando se a consulta foi bem sucedida
+    if resultado == 0:
+        # Extraindo os dados do CNPJ consultado
+        sResposta = sResposta.decode("utf-8")
+        dados = sResposta.split("|")
 
-    # Exibindo os dados do CNPJ consultado
-    print("Razão social: ", dados[1])
-    print("Nome fantasia: ", dados[2])
-    print("Endereço: ", dados[4])
-else:
-    # Exibindo o erro ocorrido na consulta
-    print("Erro na consulta: ", resultado)
+        # Exibindo os dados do CNPJ consultado
+        print("Razão social: ", dados[1])
+        print("Nome fantasia: ", dados[2])
+        print("Endereço: ", dados[4])
+    else:
+        # Exibindo o erro ocorrido na consulta
+        print("Erro na consulta: ", resultado)
+
+acbr_lib.CNPJ_Finalizar()
